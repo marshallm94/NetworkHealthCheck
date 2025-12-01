51c084ae7c5b8b73cf71aa0ece0a255ac194ffeb
Marshall McQuillen <mmcquillen@concert.co>
2025-12-01 06:33:28 -0700

    add metabase

--- startup-scripts/schema.sql
+++ startup-scripts/schema.sql
@@ -3,3 +3,16 @@ CREATE TABLE IF NOT EXISTS ping_log (
     createdateutc TIMESTAMP WITH TIME ZONE DEFAULT TIMEZONE ('UTC'::TEXT, NOW()),
     output        JSONB
 );
+CREATE VIEW view_ping_log AS (
+    SELECT
+        id,
+        createdateutc,
+        createdateutc AT TIME ZONE 'America/Denver'   AS create_date_mountain_time,
+        createdateutc AT TIME ZONE 'America/New_York' AS create_date_eAStern_time,
+        output->'packet_loss_percent'                 AS packet_loss_percent,
+        output->'round_trip_ms_max'                   AS round_trip_ms_max,
+        output->'round_trip_ms_min'                   AS round_trip_ms_min
+    FROM ping_log
+);
+
+CREATE DATABASE metabaseappdb;
